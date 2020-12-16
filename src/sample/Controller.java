c02c4c506e354cb732eed457cbac7b05b2903461
Kaj Robers <kaj.robers@gmail.com>
2020-12-16 23:11:18 +0100

    Notificatie werkt // toevoeging niet

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,39 +3,58 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
+import javafx.util.Duration;
+import org.controlsfx.control.Notifications;
 import java.io.IOException;
-import java.net.URL;
+
 
 public class Controller {
-
     @FXML
-    private BorderPane mainBorderPane;
+    public void add(javafx.event.ActionEvent actionEvent) {
+        System.out.println("Hello, World!");
 
-    @FXML
-    private void handleShowView1(ActionEvent e) {
-        loadFXML(getClass().getResource("/sample/view_1.fxml"));
-    }
-
-    @FXML
-    private void handleShowView2(ActionEvent e) {
-        loadFXML(getClass().getResource("/sample/view_2.fxml"));
-    }
-
-    @FXML
-    private void handleShowView3(ActionEvent e) {
-        loadFXML(getClass().getResource("/sample/view_3.fxml"));
-    }
-
-    private void loadFXML(URL url) {
         try {
-            FXMLLoader loader = new FXMLLoader(url);
-            mainBorderPane.setCenter(loader.load());
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("Toevoeging.fxml"));
+            /*
+             * if "fx:controller" is not set in fxml
+             * fxmlLoader.setController(NewWindowController);
+             */
+            Scene scene = new Scene(fxmlLoader.load(), 630, 400);
+            Stage stage = new Stage();
+            stage.setTitle("New Window");
+            stage.setScene(scene);
+            stage.show();
+
+
         }
+
         catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("werkt niet");
+            ;
         }
     }
+
+    @FXML
+    public void noti(javafx.event.ActionEvent actionEvent){
+        System.out.println("Notificatie");
+
+        Notifications.create()
+                .darkStyle()
+                .title("HERINERING")
+                .graphic(new Rectangle(300, 100, Color.TRANSPARENT))
+                .hideAfter(Duration.seconds(5))
+                .text("Uw bent vergeten uw medicatie in te nemen!")
+                .show();
+
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,22 +1,25 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.sql.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Toevoeging.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
         primaryStage.setTitle("Dispensio");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.setMaximized(true);
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
+
     }
 
 
