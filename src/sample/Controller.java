a87948bbd178338e4185eaaa34e4e824c3d49a21
jor11 <jor11peeters@hotmail.com>
2020-12-18 16:17:38 +0100

    DEFINITEIF 12-18-2020

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -14,11 +14,107 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.Date;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Controller {
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Label loginMessage;
+
+    public void LoginButtonOnAction(ActionEvent event) {
+        if (usernameField.getText().isBlank() == false && passwordField.getText().isBlank() == false) {
+            validateLogin();
+        } else {
+            loginMessage.setText("Please typ your information to continue");
+        }
+    }
+
+    public void cancelButtonOnAction(ActionEvent event) {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
+
+    public void validateLogin() {
+        connector connector = new connector();
+        Connection connectDbReg = connector.getConnection();
+
+        String sqlLogin = "SELECT * FROM accountgegevens WHERE Gebruikersnaam = '" + usernameField.getText() + "' AND wachtwoord ='" + passwordField.getText() +"'" ;
+
+        try {
+            Statement statement = connectDbReg.createStatement();
+            ResultSet result = statement.executeQuery(sqlLogin);
+
+            while (result.next()) {
+                if (result.getInt(1) == 1) {
+                    loginMessage.setText("Login Succesfull");
+                    openapp();
+                }
+                else {
+                    loginMessage.setText("Login Succesfull");
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+    public void openapp() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
+            Stage CreateStage =new Stage();
+            CreateStage.setTitle("Dispensio");
+            CreateStage.setScene(new Scene(root, 600, 400));
+            CreateStage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+    public void createAccount() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("register.fxml"));
+            Stage registerStage =new Stage();
+            registerStage.setTitle("Dispensio");
+            registerStage.setScene(new Scene(root, 800, 600));
+            registerStage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+    // ----------------------------------------------------------------------------------
 
     @FXML
     public void add(javafx.event.ActionEvent actionEvent) {
